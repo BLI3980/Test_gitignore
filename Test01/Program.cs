9b87594fbf472339fe1ad87cf619dfcf42d95e75
Boris Li <liba8936@protonmail.com>
2022-08-05 17:29:11 +0400

    Wrote a code for Friends task

--- Test01/Program.cs
+++ Test01/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! 1");
+﻿int count = 0, distance = 10000, FriendV1 = 1, FriendV2 = 2, dogV = 5, friend = 2, time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (FriendV1 + dogV);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (FriendV2 + dogV);
+        friend = 1;
+    }
+distance = distance - (FriendV1 + FriendV2) * time;
+count++;
+
+}
+
+Console.Write("Dog will run ");
+Console.Write(count);
+Console.Write(" times before friends meat each other");
